6ee7da5512436adb376792c6dc62a57e2f88b891
matt muniz-silva <mattmuniz-silva@Matthew.local>
2019-11-25 22:42:41 -0500

    set up vue-resource

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,5 +3,18 @@ new Vue({
   vuetify: new Vuetify(),
   data: {
     showAlert: false
+  },
+  methods: {
+    loadApi: function() {
+      this.$http
+        .get("http://jsonplaceholder.typicode.com/posts")
+        .then(this.successCallback, this.serrorCallback);
+    },
+    successCallback: function($response, { data }) {
+      console.log(response, "success");
+    },
+    errorCallback: function(response) {
+      console.log(response, "error");
+    }
   }
 });
