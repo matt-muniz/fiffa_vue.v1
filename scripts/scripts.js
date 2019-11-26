f85afbcc1f49aed8f8914705da9012228b027929
matt muniz-silva <mattmuniz-silva@Matthew.local>
2019-11-25 19:31:09 -0500

    methods & $event

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,14 @@
 new Vue({
   el: "#app",
   data: {
-    someText: "I need a beer"
+    someText: "I need a beer",
+    counter: 0
+  },
+  methods: {
+    callonClick: function(event, someArg) {
+      this.counter++;
+      console.log(someArg, event);
+    }
   },
   computed: {
     putHello: function() {
