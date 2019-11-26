81f483ac241021c3a6a720a99850d941713bee7a
matt muniz-silva <mattmuniz-silva@Matthew.local>
2019-11-25 21:47:24 -0500

    set up vuetify

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,21 +1,7 @@
 new Vue({
   el: "#app",
+  vuetify: new Vuetify(),
   data: {
-    someText: "I need a beer",
-    counter: 0
-  },
-  methods: {
-    callonClick: function(event, someArg) {
-      this.counter++;
-      console.log(someArg, event);
-    }
-  },
-  computed: {
-    putHello: function() {
-      return this.someText
-        .split("")
-        .reverse()
-        .join("");
-    }
+    showAlert: false
   }
 });
