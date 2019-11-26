1312954939bb7d798f1a6c195fcc29055d9879fa
matt muniz-silva <mattmuniz-silva@Matthew.local>
2019-11-25 19:20:46 -0500

    computed properties

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,5 +2,13 @@ new Vue({
   el: "#app",
   data: {
     someText: "I need a beer"
+  },
+  computed: {
+    putHello: function() {
+      return this.someText
+        .split("")
+        .reverse()
+        .join("");
+    }
   }
 });
